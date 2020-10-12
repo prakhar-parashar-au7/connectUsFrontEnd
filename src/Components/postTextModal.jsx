04830e9d2b2aee34642271f16a71e84f454fe111
Prakhar <prakhar24680@gmail.com>
2020-10-13 04:43:26 +0530

    a

--- src/Components/postTextModal.jsx
+++ src/Components/postTextModal.jsx
@@ -94,7 +94,7 @@ const photoStyle = {
             Add Photo
             </button>
              :
-               <Image publicId={photoInfo} cloudName="prakhar-parashar" width="300" height="400"/> } 
+               <Image publicId={photoInfo} cloudName="prakhar-parashar" width="400" height="400"/> } 
          
          </div>
       
